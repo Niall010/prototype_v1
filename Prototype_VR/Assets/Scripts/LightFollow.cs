b4e91f26567839506ada9c228a92e906ffcd7c67
James Swan <JamesRSwan96@gmail.com>
2018-05-15 20:46:31 +0100

    LightFollow
    Created the scene LightTestScene, and created script LightFollow for making a light follow a specified object.

---
+++ Prototype_VR/Assets/Scripts/LightFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFollow : MonoBehaviour
+{
+    public Transform objectToFollow;
+	
+	void Update ()
+    {
+        // If this object's position isn't equal to the object to follow's position
+        // Set this object's position to same position of object to follow
+		if (gameObject.transform.position != objectToFollow.position)
+        {
+            gameObject.transform.position = objectToFollow.position;
+        }
+	}
+}
